ff01393d8806323fce85e306f9064b0215aef4b2
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2026-09-03 22:08:28 +0530

    added new icon files to the assets for the application

--- src/assets.py
+++ src/assets.py
@@ -6,3 +6,6 @@ _RP = ASSETS_PATH
 
 
 DEFAULT_RESULT_ICON_PATH: Path = _RP / "indeterminate_question_box.svg"
+APP_ICON_SVG: Path = _RP / "icon" / "icon.svg"
+APP_ICON_PNG: Path = _RP / "icon" / "icon.png"
+APP_ICON_ICO: Path = _RP / "icon" / "icon.ico"
